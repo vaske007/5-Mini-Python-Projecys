586251ce7bf1430f742bc31b0a82bfffa17a8524
Vasilije Mrmak <vmrmak36@gmail.com>
2022-09-01 16:58:42 +0100

    More information about the random.randrange

--- number_gusser.py
+++ number_gusser.py
@@ -1,3 +1,5 @@
 import random
 
-random.randrange(-1, 10) #This is the range you want your random numbers to be in
+r = random.randrange(-5, 10) #This is the range you want your random numbers to be in. The number 10 can not be generated.
+
+print(r)
