e3e181ca8808ac4ba238da9a38f0091f1ac41144
Vasilije Mrmak <vmrmak36@gmail.com>
2022-09-01 17:19:00 +0100

    Letting the user input a number, and checking if it is a number or not.

--- number_gusser.py
+++ number_gusser.py
@@ -1,5 +1,17 @@
+# randint does the same thing as randrange, execpt that it does include the last number you want in range
 import random
 
-r = random.randrange(-5, 10) #This is the range you want your random numbers to be in. The number 10 can not be generated.
+top_of_range = input("Type a number: ") # Whatever the user types in, will be returned to us as a string(" "), which in python is not considered a number, integer... it's considered a string.
 
-print(r)
+if top_of_range.isdigit(): # .isdigit checks wheter or not the string that the user types in is a number, digit, and if so it will activate the if function.
+    top_of_range = int(top_of_range) # Here we are converting the string that we get from the user to a number. If the user types in "25", the int function will turn it into 25. int("25). But if the user types in text like "hello", the int will return error.
+
+    if top_of_range <= 0:
+        print("Please type a number larger than  0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range) 
+
