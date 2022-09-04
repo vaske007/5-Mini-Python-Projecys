7c3fc6bbe6b05d9822f78a2e981135fdbe166b44
Vasilije Mrmak <vmrmak36@gmail.com>
2022-09-04 20:51:20 +0100

    Inital Commit

---
+++ chooser.py
@@ -0,0 +1,8 @@
+import random
+
+time_to_code = ["30 min", "45 min", "1 hour", "1 hour 15 min", "1 hour 30 min", "1 hour 45 min", "2 hour"]
+
+how_much_time_do_you_have = input("How much time do you have? ")
+if how_much_time_do_you_have == "30 min":
+    print(time_to_code[0])
+
